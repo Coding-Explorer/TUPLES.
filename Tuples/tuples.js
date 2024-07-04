c6d529dfb33d78b12e278348cd1136323069489d
Coding-Explorer <160587282+Coding-Explorer@users.noreply.github.com>
2024-07-04 01:04:35 -0700

    Add files via upload

---
+++ Tuples/tuples.js
@@ -0,0 +1,54 @@
+/////////////////TUPLES///////////////////.
+/*  Tuples are a collection of values of different data types stored in a single entity, allowing for the storage
+and  manipulation of multiple values as a single unit. tuples are similar to arrays , but with a fixed size and
+immutable elements. */
+// A tuple is a typed array with a pre-defined length and types for each index.
+// Tuples are great because they allow each element in the array to be a known type of value.
+// Typescript introduced a new data type called a tuple.
+// Tuples allow you to define an array with a fixed number of elements, where each element has a known type.
+// Using tuples can help catch bugs early on in development.
+// Tuples are a way to enforce a fixed length and type structure on arrays.
+// Here's i'm gonna to Create a tuple.
+var person = ['Asif khan', 22, true];
+// Now Accessing elements.
+console.log(person[0]); // Output: Asif khan.
+console.log(person[1]); // Output: 22.
+console.log(person[2]); // Output: true.
+// Now Modifying elements.
+person[0] = 'Adnan khan';
+person[1] = 24;
+person[2] = true;
+console.log(person[0]); // Output: Adnan khan.
+console.log(person[1]); // Output: 24.
+console.log(person[2]); // Output: true.
+// Now Adding new elements.
+person.push('Danish khan');
+console.log(person); // Output: Danish khan.
+// Now Removing elements.
+person.pop();
+console.log(person); // Output: Adnan khan.
+// Now seeing the usage of Tuple with different data types.
+var person1 = ['Asif khan', 22, true, 'Danish khan', 24];
+console.log(person1); // Output: Asif khan, 22, true, Danish khan, 24.
+// Example no 02.
+var colors = [1, 'red', true];
+console.log(colors); // Output: [1, 'red', true].
+// Example no 03.
+var fruits = ['pineapple', true];
+console.log(fruits); // Output: ['pineapple', true].
+// Example no 04.
+// Now exploring the use of tuples in the function.
+function printInfo(person) {
+    console.log("Name: ".concat(person[0]));
+    console.log("Age: ".concat(person[1]));
+    console.log("Is Active: ".concat(person[2]));
+}
+printInfo(['Asif khan', 22, true]);
+// Example no 05.
+// Now exploring the use of tuples in objects.
+var person0 = {
+    name: 'Ahmed khan',
+    age: 25,
+    isActive: false
+};
+console.log(person0); // Output: {name: 'Ahmed khan', age: 25, isActive: false}.
